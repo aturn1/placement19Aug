85ec83c91d7fa94d18792bbbf60371ab0b3a6e9a
prashanth <nocturnaldevops@gmail.com>
2024-09-02 06:19:40 +0000

    first commit - added installation scripts

---
+++ tomcat_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y tomcat9 tomcat9-admin # if the targets are ubuntu 22.04
+# in case of 24.04  the package names to be changed to tomcat10 and tomcat10-admin
